c452060072f43d84567d6d21ccaab4e40d439354
Gaurav Ghai <htmlhints@gmail.com>
2020-11-06 23:21:18 +0530

    workshop files

--- index.php
+++ index.php
@@ -1,15 +1,27 @@
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+<style>
+    .sdev {
+        wef
+    }
+</style>
 <?php
 include 'inc/config.php';
+include 'inc/header.php';
 $sql = "SELECT * FROM test";
 $result = $link->query($sql);
 if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
         // var_dump($row);
-        echo $row['id']." ".$row['head'].'<br>';
+        echo '<div class="card" style="width: 18rem;">
+        <div class="card-body">
+          <h5 class="card-title sdev">'.$row["head"].'</h5>
+          <p class="card-text">'.$row["description"].'</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>';
     }
 }
 // ending point
 
 ?>
+
