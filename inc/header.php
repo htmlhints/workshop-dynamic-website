eab1efdd643dfb7fbde9b701a0bf66dea9242d9a
Gaurav Ghai <htmlhints@gmail.com>
2020-11-08 19:29:23 +0530

    updated code

--- inc/header.php
+++ inc/header.php
@@ -5,6 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Workshop</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+    <script src="./bootstrap/4.4.1/js/bootstrap.min.js"></script>
 </head>
 <body>
--- index.php
+++ index.php
@@ -10,10 +10,12 @@ $sql = "SELECT * FROM test";
 $result = $link->query($sql);
 if ($result->num_rows > 0) {
     // output data of each row
+    echo '<div class="row">';
     while($row = $result->fetch_assoc()) {
         // var_dump($row);
         echo '<div class="card" style="width: 18rem;">
         <div class="card-body">
+            <img src="'.$row["image"].'" width="100%">
           <h5 class="card-title sdev">'.$row["head"].'</h5>
           <p class="card-text">'.$row["description"].'</p>
           <a href="#" class="btn btn-primary">Go somewhere</a>
