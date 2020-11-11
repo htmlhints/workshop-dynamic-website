2362ad14e2386225f105eb74ad0366cb2302ef3c
Gaurav Ghai <htmlhints@gmail.com>
2020-11-11 19:19:20 +0530

    workshop code

--- index.php
+++ index.php
@@ -13,14 +13,17 @@ if ($result->num_rows > 0) {
     echo '<div class="row container mx-auto pt-5 pb-5">';
     while($row = $result->fetch_assoc()) {
         // var_dump($row);
-        echo '<div class="card col-4" style="background-color:coral;">
-        <div class="card-body">
-            <img src="'.$row["image"].'" width="100%">
-          <h5 class="card-title sdev">'.$row["head"].'</h5>
-          <p class="card-text">'.$row["description"].'</p>
-          <a href="#" class="btn btn-primary">Go somewhere</a>
-        </div>
-      </div>';
+        echo '
+        <div class="col-lg-4 col-md-6 col-12 py-2">
+            <div class="card" style="background-color:coral;">
+                <div class="card-body">
+                    <img src="'.$row["image"].'" width="100%">
+                    <h5 class="card-title sdev">'.$row["head"].'</h5>
+                    <p class="card-text">'.$row["description"].'</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+        </div>';
     }
 }
 // ending point
