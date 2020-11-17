5927cac79afc44e6056fb9a9f00eb4d0b896ab70
Gaurav Ghai <htmlhints@gmail.com>
2020-11-17 19:29:25 +0530

    workshop

--- inc/footer.php
+++ inc/footer.php
@@ -1,16 +1,17 @@
 </div>
 <footer class="py-4" style="background:black;">
+<form class="newsletter my-3 col-4 mx-auto" method="post" id="contact_modal_form" onsubmit="return false;">
+    <h3 class="text-center text-white mb-3">Join Our Newsletter</h3>
+    <input type="email" name="news_email" id="news_email"  class="form-control" placeholder="Your Email Address" value="">
+    <button class="my-3 btn btn-primary" id="element">
+        Subscribe
+    </button>
+</form>
 <div class="col-12 text-center copyright">
     <p class="mb-0 mt-1 text-white" style="font-size: 16px;">
         &copy; <?php echo date("Y");?> <span class="read-more">Workshop</span> - All Rights Reserved	
     </p>
 </div>
-<form class="newsletter my-3" method="post" id="contact_modal_form" onsubmit="return false;">
-        <input type="email" name="news_email" id="news_email"  class="form-control" placeholder="Your Email Address" value="">
-        <button class="my-3" id="element">
-            Subscribe
-        </button>
-</form>
 <!-- <form class="newsletter my-3" method="get"  action="https://localhost/workshop/inc/ajax/form.php" >
         <input type="email" name="news_email" id="news_email"  class="form-control" placeholder="Your Email Address" value="">
         <button class="my-3" id="element">
