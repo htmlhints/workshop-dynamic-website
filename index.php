16bba760c4967b88a5ee9b8d6543276bf2bc0e99
Gaurav Ghai <gauravghai76@gmail.com>
2020-12-20 19:24:07 +0530

    latest code

--- index.php
+++ index.php
@@ -1,4 +1,45 @@
 <?php
-echo 'hi';
+include 'inc/header.php';
 ?>
-
+<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="https://i3.ytimg.com/vi/qUlk8q-lvgA/maxresdefault.jpg" class="d-block w-100" alt="https://i3.ytimg.com/vi/qUlk8q-lvgA/maxresdefault.jpg">
+    </div>
+    <div class="carousel-item">
+      <img src="https://i3.ytimg.com/vi/qUlk8q-lvgA/maxresdefault.jpg" class="d-block w-100" alt="https://i3.ytimg.com/vi/qUlk8q-lvgA/maxresdefault.jpg">
+    </div>
+    <div class="carousel-item">
+      <img src="https://i3.ytimg.com/vi/qUlk8q-lvgA/maxresdefault.jpg" class="d-block w-100" alt="https://i3.ytimg.com/vi/qUlk8q-lvgA/maxresdefault.jpg">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+<div class="d-flex pb-5 mt-5">
+    <div class="card col-4 mx-2">
+        <img src="https://i3.ytimg.com/vi/qUlk8q-lvgA/maxresdefault.jpg" class="card-img-top" alt="https://i3.ytimg.com/vi/qUlk8q-lvgA/maxresdefault.jpg">
+        <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+    </div>
+    <div class="card col-4 mx-2">
+        <img src="https://i3.ytimg.com/vi/qUlk8q-lvgA/maxresdefault.jpg" class="card-img-top" alt="https://i3.ytimg.com/vi/qUlk8q-lvgA/maxresdefault.jpg">
+        <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+    </div>
+</div>
+<?php
+include 'inc/footer.php';
+?>
